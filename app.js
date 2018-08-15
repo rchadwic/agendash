65b0d1508eab916507e78af8aac4fc796ee79c9d
Rob Chadwick <rchadwic@gmail.com>
2018-08-15 11:28:28 -0400

    more path stuff

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 'use strict';
 const path = require('path');
 const middleware = require('./lib/middlewares/express');
+const agendadash = require('./lib/agendash');
 module.exports = (agenda, options) => {
   options = options || {};
   if (!options.middleware) {
     options.middleware = 'express';
   }
 
-  const agendash = require('./lib/agendash')(agenda, options);
+  const agendash = agendadash(agenda, options);
 
  
     console.log("startup agendadash");
